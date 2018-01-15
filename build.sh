d1015c51f5f33df9c95579b76fa5a859a17ca755
Fabrice Desré <fabrice@desre.org>
2018-01-14 23:32:20 -0800

    Commit Cargo.lock

--- build.sh
+++ build.sh
@@ -17,9 +17,10 @@ export GONK_PRODUCT=$GONK_PRODUCT_NAME
 # export NDK_ANDROID_VERSION=18
 # export ANDROID_NDK=$GONK_DIR/prebuilts/ndk/current
 
-export CPPFLAGS="-DANDROID -DTARGET_OS_GONK \
+export CFLAGS="-DANDROID -DTARGET_OS_GONK \
 -DANDROID_VERSION=18 \
 -DGR_GL_USE_NEW_SHADER_SOURCE_SIGNATURE=1 \
+-isystem $GONK_DIR/bionic \
 -isystem $GONK_DIR/bionic/libc/$ARCH_DIR/include \
 -isystem $GONK_DIR/bionic/libc/include/ \
 -isystem $GONK_DIR/bionic/libc/kernel/common \
@@ -28,16 +29,15 @@ export CPPFLAGS="-DANDROID -DTARGET_OS_GONK \
 -I$GONK_DIR/system \
 -I$GONK_DIR/system/core/include \
 -I$GONK_DIR/frameworks/native/opengl/include \
--I$GONK_DIR/external/zlib "
+-I$GONK_DIR/external/zlib"
 
-export CXXFLAGS="-O2 -mandroid -fPIC -std=c++11 \
+export CPPFLAGS="-O2 -mandroid -fPIC \
 -isystem $GONK_DIR/api/cpp/include \
 -isystem $GONK_DIR/external/stlport/stlport \
--isystem $GONK_DIR/bionic \
 -isystem $GONK_DIR/bionic/libstdc++/include \
-$CPPFLAGS"
+$CFLAGS"
 
-export CFLAGS=$CXXFLAGS
+export CXXFLAGS="$CPPFLAGS -std=c++11"
 
 export LDFLAGS="-mandroid -L$GONK_DIR/out/target/product/$GONK_PRODUCT_NAME/obj/lib \
 -Wl,-rpath-link=$GONK_DIR/out/target/product/$GONK_PRODUCT_NAME/obj/lib \
