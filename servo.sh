8107ecd564400ef5692fb26685067ef429bca017
Fabrice Desré <fabrice@desre.org>
2018-02-20 12:39:45 -0800

    Don't hardcode url in the start script

--- servo.sh
+++ servo.sh
@@ -4,8 +4,4 @@
 # resources/* files properly.
 cd /data/local/servo
 
-./servo file:///data/local/servo/index.html $@
-
-# ./servo https://en.wikipedia.org/wiki/Main_Page $@
-# ./servo file:///data/local/servo/ui/index.html $@
-# ./servo "https://www.google.com/maps?force=qVTs2FOxxTmHHo79-pwa&source=mlapk"
+./servo $@
