b3da85af407396341d05f1734d1b52bda0119165
Fabrice Desré <fabrice@desre.org>
2018-02-20 12:50:45 -0800

    Fold the bootstrap script into the main build script

--- build.sh
+++ build.sh
@@ -13,6 +13,14 @@ fi
 
 export RUST_TARGET=armv7-linux-androideabi
 
+# Update the Rust toolchain if needed.
+# TODO: checkt the current version and only call rustup if needed.
+RUST_VERSION=`cat rust-toolchain`
+
+rustup install $RUST_VERSION
+rustup target add $RUST_TARGET
+rustup override set $RUST_VERSION
+
 export PATH=$GONK_DIR/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/:$PATH
 
 SYSROOT=$GONK_DIR/prebuilts/ndk/current/platforms/android-18/arch-arm/
